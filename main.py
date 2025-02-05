76c0132c302dd98691e52d791e4b49e249cad5fb
lilleole <burhennemalte1@gmail.com>
2025-02-05 12:44:56 +0100

    made it better

--- main.py
+++ main.py
@@ -1,42 +1,50 @@
-def wordCount(text):
-    return len(text.split())
-
-def totalChar(text):
-    return len(text)
-
-def charCount(text):
-    dictionary = {}
-    for char in text:
-        if char not in dictionary:
-            dictionary[char] = 1
-        else:
-            dictionary[char] += 1
-    return dictionary
+class bookbot:
+    def __init__(self,file):
+        self.path = file
+        self.f = open(self.path, "r")
+        self.text = self.f.read()
+        self.dictionary = self.charCount()
+        self.totalChar = self.totalChar()
+        self.wordCount = self.wordCount()
 
 
-def printOutput(path: str, totalChar: int, wordCount: int, charCount: dict) -> None:
-    dictlist = []
-    for key, value in charCount.items():
-        temp = [key, value]
-        dictlist.append(temp)
-    sortedlist = sorted(dictlist, key=lambda x: x[1], reverse=True)
-
-
-    print(f"--- Begin report of {path} ---")
-    print(f"{wordCount} words found in the document")
-    print(f"{totalChar} characters found in the document\n")
-
-    for item in sortedlist:
-        print(f"The '{item[0]}' character was found {item[1]} times")
-
-    print("--- End report ---")
+    def wordCount(self):
+        return len(self.text.split())
+    
+    def totalChar(self):
+        return len(self.text)
+    
+    def charCount(self):
+        self.dictionary = {}
+        for char in self.text:
+            if char not in self.dictionary:
+                self.dictionary[char] = 1
+            else:
+                self.dictionary[char] += 1
+        return self.dictionary
+    
+    
+    def printOutput(self) -> None:
+        dictlist = []
+        for key, value in self.dictionary.items():
+            temp = [key, value]
+            dictlist.append(temp)
+        sortedlist = sorted(dictlist, key=lambda x: x[1], reverse=True)
+    
+    
+        print(f"--- Begin report of {self.path} ---")
+        print(f"{self.wordCount} words found in the document")
+        print(f"{self.totalChar} characters found in the document\n")
+    
+        for item in sortedlist:
+            print(f"The '{item[0]}' character was found {item[1]} times")
+    
+        print("--- End report ---")
 
 def main():
-    path = "frankenstein.txt"
-    f = open(path,"r")
-    text = f.read()
+    frankenstein = bookbot("frankenstein.txt")
 
-    printOutput(path, totalChar(text), wordCount(text.lower()), charCount(text.lower()))
+    frankenstein.printOutput()
 
 
 
